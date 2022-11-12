46fecd9adfd46e0c69ad6d431bab4636a094cf3f
theodorrodeoht <theodorrodeoht@gmail.com>
2022-11-12 13:36:56 +0300

    Update figures

---
+++ utils/install_r_libraries.r
@@ -0,0 +1,6 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("ChIPseeker")
+BiocManager::install("GenomicFeatures")
+BiocManager::install("ChIPpeakAnno")
+BiocManager::install("clusterProfiler")
